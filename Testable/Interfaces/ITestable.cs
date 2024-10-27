2917069ee51db4d2df50e86f8b27a1ab803948c3
John <zerihal@hotmail.com>
2024-10-27 22:53:31 +0000

    Added comments

--- Testable/Interfaces/ITestable.cs
+++ Testable/Interfaces/ITestable.cs
@@ -2,14 +2,48 @@
 {
     public interface ITestable
     {
+        /// <summary>
+        /// Testable internal ID.
+        /// </summary>
         int ID { get; set; }
+
+        /// <summary>
+        /// Testable unique ID.
+        /// </summary>
         Guid TestableID { get; }
+
+        /// <summary>
+        /// Testable class name.
+        /// </summary>
         string? Name { get; }
+
+        /// <summary>
+        /// Testable class description.
+        /// </summary>
         string? Description { get; }
+
+        /// <summary>
+        /// Author.
+        /// </summary>
         string? Author { get; }
+
+        /// <summary>
+        /// Class methods.
+        /// </summary>
         IEnumerable<string> Methods { get; }
+
+        /// <summary>
+        /// Class properties.
+        /// </summary>
         IEnumerable<string> Properties { get; }
 
+        /// <summary>
+        /// Executes the given method with any arguments passed.
+        /// </summary>
+        /// <param name="method">Method name.</param>
+        /// <param name="args">Arguments.</param>
+        /// <param name="error">Execution error.</param>
+        /// <returns>Method return value/object (if any).</returns>
         object ExecuteMethod(string method, object[] args, out string error);
     }
 }
